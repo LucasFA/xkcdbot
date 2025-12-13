09845d18e365ccd2970119a621edba8f49e6ebc8
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-12-13 15:03:00 +0000

    subcribers: delete everything

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ import json
 import time
 from functools import wraps
 
-subscriberChats = []
 latestComic = 0
-subscribersFile = "subscribers.json"
-
 
 def load_env(path=".env"):
     with open(path) as f:
@@ -32,32 +29,7 @@ async def start(update, context) -> None:
         "/random - I'll send you a random xkcd."
     )
 
-
-def subscribe(bot, update):
-    global subscriberChats
-    if update.message.chat_id not in subscriberChats:
-        subscriberChats.append(update.message.chat_id)
-        update.message.reply_text(
-            "You are now subscribed.\n"
-            "From now on I will send you new xkcd comics when they are released.\n"
-            "If you wish to unsubscribe, send me a /unsubscribe command."
-        )
-    else:
-        update.message.reply_text(
-            "Hey you're already subscribed!\nDid you mean /unsubscribe?"
-        )
-
-
-def unsubscribe(bot, update):
-    global subscriberChats
-    if update.message.chat_id in subscriberChats:
-        subscriberChats.remove(update.message.chat_id)
-        update.message.reply_text("You have been unsubscribed.")
-    else:
-        update.message.reply_text("Hey you're not even subscribed to begin with!")
-
-
-async def xkcd(update, context):
+async def xkcd(update, context) -> None:
     if update.message.text == "/xkcd" or update.message.text == "/xkcd latest":
         setLatestComic()
         comic = getLatestComic()
@@ -71,10 +43,8 @@ async def xkcd(update, context):
         comic = getComic(xkcdNumber)
         await send_comic(update, comic)
 
-async def random(update, context):
+async def random(update, context) -> None:
     setLatestComic()
-    print(latestComic)
-    print("HEREEEE")
     await send_comic(update, getRandomComic(latestComic))
 
 async def send_comic(update, comic):
@@ -84,39 +54,10 @@ async def send_comic(update, comic):
     )
 
 
-def comicNotify(bot, job):
-    comic = getLatestComic()
-    if comic["num"] > latestComic:
-        setLatestComic()
-        for chat_id in subscriberChats:
-            send_message_retry(bot, chat_id, "A new xkcd is out!")
-            send_photo_retry(bot, chat_id, comic["img"])
-            send_message_retry(
-                bot,
-                chat_id,
-                "xkcd " + str(comic["num"]) + "\nAlt-text: " + comic["alt"],
-            )
-
-
 def setLatestComic():
     global latestComic
     comic = getLatestComic()
     latestComic = comic["num"]
-
-
-def readSubscribers():
-    global subscriberChats
-    with open(subscribersFile, "r") as infile:
-        subscriberJson = json.load(infile)
-        subscriberChats = subscriberJson["subscribers"]
-
-
-def saveSubscribers(bot, job):
-    with open(subscribersFile, "w") as outfile:
-        finalJson = {}
-        finalJson["subscribers"] = subscriberChats
-        json.dump(finalJson, outfile)
-
 
 def main():
     # First load our subscriber list if it exists:
@@ -137,15 +78,13 @@ def main():
 
     token = os.getenv("TELEGRAM_TOKEN")
     if not token:
-        raise RuntimeError("Not Telegram token provided. Add one to the `.env` file")
+        raise RuntimeError("No Telegram token provided. Add one to the `.env` file")
     # updater = Updater(token)
     app = ApplicationBuilder().token(token).build()
 
-    # Create dispatcher and register commands.
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", start))
-    app.add_handler(CommandHandler("commands", start))
     # app.add_handler(CommandHandler("subscribe", subscribe))
     # app.add_handler(CommandHandler("unsubscribe", unsubscribe))
     app.add_handler(CommandHandler("xkcd", xkcd))
